b849e34556d9f856bec966b9e4a6c9c573bf3bdc
owdaro <owdaro@owdaro.com>
2016-06-13 11:01:35 +0400

    a

--- OFrameLibrary/Models/JSONConfirmation.cs
+++ OFrameLibrary/Models/JSONConfirmation.cs
@@ -2,6 +2,14 @@
 {
     public class JSONConfirmation
     {
+        public JSONConfirmation() { }
+
+        public JSONConfirmation(bool isSuccess, string message)
+        {
+            Message = message;
+            IsSuccess = isSuccess;
+        }
+
         public string Message { get; set; }
         public bool IsSuccess { get; set; }
         public object Object { get; set; }
