8c12c7645157bcc6f7d6db6af21e6583f9d89143
owdaro <owdaro@owdaro.com>
2016-06-17 01:40:07 +0400

    a

--- OFrameLibrary/Models/FileUploadSettings.cs
+++ OFrameLibrary/Models/FileUploadSettings.cs
@@ -3,14 +3,23 @@
     public class FileUploadSettings
     {
         /// <summary>
-        /// Max File Size In MB
+        /// Max File Size In MB. Default to 5MB
         /// </summary>
-        public int MaxSize { get; set; }
+        public int MaxSize { get; set; } = 5;
 
-        public string StoragePath { get; set; }
+        /// <summary>
+        /// Path where to store the file on the server. Default to ~/Storage/
+        /// </summary>
+        public string StoragePath { get; set; } = LocalStorages.Storage;
 
-        public FileUploadMessageSettings Messages { get; set; }
+        /// <summary>
+        /// File Upload status message settings
+        /// </summary>
+        public FileUploadMessageSettings Messages { get; set; } = new FileUploadMessageSettings();
 
+        /// <summary>
+        /// Type of file such as PDF, Images or Docs
+        /// </summary>
         public FileType FileType { get; set; } = FileType.All;
     }
 }
