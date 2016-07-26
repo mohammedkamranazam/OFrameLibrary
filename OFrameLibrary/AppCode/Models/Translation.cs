ed24d7153081e25c6d1baf60082963187c3b3d0f
Arrowtec <Arrowtec@DESKTOP-O564VVP>
2016-07-26 09:11:48 +0400

    a

---
+++ OFrameLibrary/AppCode/Models/Translation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OFrameLibrary.Models
+{
+    public class Translation
+    {
+        public string Locale { get; set; }
+        public string Text { get; set; }
+    }
+}
--- OFrameLibrary/AppCode/SettingsHelpers/LanguageHelper.cs
+++ OFrameLibrary/AppCode/SettingsHelpers/LanguageHelper.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Xml;
+using System.Linq;
+
 
 namespace OFrameLibrary.SettingsHelpers
 {
@@ -399,6 +401,25 @@ namespace OFrameLibrary.SettingsHelpers
                     }
                 }
             }
+        }
+
+        public static string GetLocaleName(string locale)
+        {
+            return LanguageHelper.GetLanguages().FirstOrDefault(c => c.Locale == locale)?.Name;
+        }
+
+        public static string GetLocaleHash(string locale)
+        {
+            return string.Format("{0}#{1};", locale, GetLocaleName(locale));
+        }
+
+        public static string GetLocalesHash(List<Translation> translation)
+        {
+            var locales = translation.Select(c => c.Locale).ToList();            
+
+            locales = locales.Select(c => GetLocaleHash(c)).ToList();
+
+            return locales.ToArray<string>().Join("");
         }
     }
 }
