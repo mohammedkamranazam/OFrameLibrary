1bdd244b48f1f25865fe8a42f9c56c2f6761a6d7
Kamran <Kamran@KAMRAN-PC>
2016-10-30 16:50:43 +0530

    a

--- OFrameLibrary/AppCode/Models/JSONConfirmation.cs
+++ OFrameLibrary/AppCode/Models/JSONConfirmation.cs
@@ -12,8 +12,16 @@
             IsSuccess = isSuccess;
         }
 
+        public JSONConfirmation(bool isSuccess, bool isError, string message)
+        {
+            Message = message;
+            IsSuccess = isSuccess;
+            IsError = isError;
+        }
+
         public string Message { get; set; }
         public bool IsSuccess { get; set; }
+        public bool IsError { get; set; }
         public object Object { get; set; }
     }
 }
