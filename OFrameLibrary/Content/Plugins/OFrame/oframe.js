0741f9a6894a0e5168e86b90b9b1d933781150be
Kamran <Kamran@KAMRAN-PC>
2017-02-14 21:33:01 +0530

    a

--- OFrameLibrary/Content/Plugins/OFrame/oframe.js
+++ OFrameLibrary/Content/Plugins/OFrame/oframe.js
@@ -24,6 +24,10 @@ Array.prototype.indexOf = function (obj, fromIndex) {
     return -1;
 };
 
+Date.prototype.format = function (mask, utc) {
+    return dateFormat(this, mask, utc);
+};
+
 $.fn.clearErrors = function () {
     $(this).each(function () {
         $(this).removeClass("input-validation-error");
@@ -33,6 +37,14 @@ $.fn.clearErrors = function () {
 };
 
 var oframe = {
+    toDate: function (dateStr, spltChar) {
+        var spch = "/";
+        if (spltChar) {
+            spch = spltChar;
+        }
+        var parts = dateStr.split(spch);
+        return new Date(parts[2], parts[1] - 1, parts[0]);
+    },
     treatAsUTC: function (date) {
         var result = new Date(date);
         result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
@@ -127,14 +139,50 @@ var oframe = {
         $(tabContent).parents(".tc-tabs").find(".nav-tabs li a[data-tabid=" + $(tabContent).attr("id") + "]").click();
     },
     updateUrl: function (uri, key, value) {
+        //var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+        //var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+        //if (uri.match(re)) {
+        //    return uri.replace(re, '$1' + key + "=" + value + '$2');
+        //}
+        //else {
+        //    return uri + separator + key + "=" + value;
+        //}
+
+        var i = uri.indexOf('#');
+        var hash = i === -1 ? '' : uri.substr(i);
+        uri = i === -1 ? uri : uri.substr(0, i);
+
         var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
         var separator = uri.indexOf('?') !== -1 ? "&" : "?";
         if (uri.match(re)) {
-            return uri.replace(re, '$1' + key + "=" + value + '$2');
+            uri = uri.replace(re, '$1' + key + "=" + value + '$2');
+        } else {
+            uri = uri + separator + key + "=" + value;
         }
-        else {
-            return uri + separator + key + "=" + value;
-        }
+        return uri + hash;  // finally append the hash as well
+
+        //REMOVE KEY VALUE PAIR IF VALUE IS EMPTY
+        //var i = uri.indexOf('#');
+        //var hash = i === -1 ? '' : uri.substr(i);
+        //uri = i === -1 ? uri : uri.substr(0, i);
+
+        //var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+        //var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+
+        //if (!value) {
+        //    // remove key-value pair if value is empty
+        //    uri = uri.replace(new RegExp("([?&]?)" + key + "=[^&]*", "i"), '');
+        //    if (uri.slice(-1) === '?') {
+        //        uri = uri.slice(0, -1);
+        //    }
+        //    // replace first occurrence of & by ? if no ? is present
+        //    if (uri.indexOf('?') === -1) uri = uri.replace(/&/, '?');
+        //} else if (uri.match(re)) {
+        //    uri = uri.replace(re, '$1' + key + "=" + value + '$2');
+        //} else {
+        //    uri = uri + separator + key + "=" + value;
+        //}
+        //return uri + hash;
     },
     select: function (arg, field) {
         var value = $(field).val();
@@ -840,3 +888,109 @@ var oframe = {
 
     $.fn.LocalizeForm = localizeForm;
 }(jQuery));
+
+
+var dateFormat = function () {
+    var token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloSZ]|"[^"]*"|'[^']*'/g,
+        timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
+        timezoneClip = /[^-+\dA-Z]/g,
+        pad = function (val, len) {
+            val = String(val);
+            len = len || 2;
+            while (val.length < len) val = "0" + val;
+            return val;
+        };
+
+    // Regexes and supporting functions are cached through closure
+    return function (date, mask, utc) {
+        var dF = dateFormat;
+
+        // You can't provide utc if you skip other args (use the "UTC:" mask prefix)
+        if (arguments.length == 1 && Object.prototype.toString.call(date) == "[object String]" && !/\d/.test(date)) {
+            mask = date;
+            date = undefined;
+        }
+
+        // Passing date through Date applies Date.parse, if necessary
+        date = date ? new Date(date) : new Date;
+        if (isNaN(date)) throw SyntaxError("invalid date");
+
+        mask = String(dF.masks[mask] || mask || dF.masks["default"]);
+
+        // Allow setting the utc argument via the mask
+        if (mask.slice(0, 4) == "UTC:") {
+            mask = mask.slice(4);
+            utc = true;
+        }
+
+        var _ = utc ? "getUTC" : "get",
+            d = date[_ + "Date"](),
+            D = date[_ + "Day"](),
+            m = date[_ + "Month"](),
+            y = date[_ + "FullYear"](),
+            H = date[_ + "Hours"](),
+            M = date[_ + "Minutes"](),
+            s = date[_ + "Seconds"](),
+            L = date[_ + "Milliseconds"](),
+            o = utc ? 0 : date.getTimezoneOffset(),
+            flags = {
+                d: d,
+                dd: pad(d),
+                ddd: dF.i18n.dayNames[D],
+                dddd: dF.i18n.dayNames[D + 7],
+                m: m + 1,
+                mm: pad(m + 1),
+                mmm: dF.i18n.monthNames[m],
+                mmmm: dF.i18n.monthNames[m + 12],
+                yy: String(y).slice(2),
+                yyyy: y,
+                h: H % 12 || 12,
+                hh: pad(H % 12 || 12),
+                H: H,
+                HH: pad(H),
+                M: M,
+                MM: pad(M),
+                s: s,
+                ss: pad(s),
+                l: pad(L, 3),
+                L: pad(L > 99 ? Math.round(L / 10) : L),
+                t: H < 12 ? "a" : "p",
+                tt: H < 12 ? "am" : "pm",
+                T: H < 12 ? "A" : "P",
+                TT: H < 12 ? "AM" : "PM",
+                Z: utc ? "UTC" : (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
+                o: (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4),
+                S: ["th", "st", "nd", "rd"][d % 10 > 3 ? 0 : (d % 100 - d % 10 != 10) * d % 10]
+            };
+
+        return mask.replace(token, function ($0) {
+            return $0 in flags ? flags[$0] : $0.slice(1, $0.length - 1);
+        });
+    };
+}();
+
+dateFormat.masks = {
+    "default": "ddd mmm dd yyyy HH:MM:ss",
+    shortDate: "m/d/yy",
+    mediumDate: "mmm d, yyyy",
+    longDate: "mmmm d, yyyy",
+    fullDate: "dddd, mmmm d, yyyy",
+    shortTime: "h:MM TT",
+    mediumTime: "h:MM:ss TT",
+    longTime: "h:MM:ss TT Z",
+    isoDate: "yyyy-mm-dd",
+    isoTime: "HH:MM:ss",
+    isoDateTime: "yyyy-mm-dd'T'HH:MM:ss",
+    isoUtcDateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'"
+};
+
+dateFormat.i18n = {
+    dayNames: [
+        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ],
+    monthNames: [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ]
+};
